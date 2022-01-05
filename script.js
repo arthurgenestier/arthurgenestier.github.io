76e2fade059b05d49fd51af55bbf6d9e99707caf
arthurgenestier <genestier.arthur@gmail.com>
2022-01-05 12:53:58 +0100

    son

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function playAudio(url) {
     new Audio(url).play();
 }
+
 function lightLamp() {
 var element = document.getElementById("lamp");
 element.classList.toggle("jaune");
 } 
+
 function lightBody() {
 var element = document.getElementById("body");
 element.classList.toggle("body-light");
 }
+
 function lightPoster() {
-var element = document.getElementById("poster");
-element.classList.toggle("poster-light");
+var element = document.getElementById("objects");
+element.classList.toggle("light");
 }
+
 function posterRotate() {
 var element = document.getElementById("poster");
 element.classList.toggle("poster-rotate");
