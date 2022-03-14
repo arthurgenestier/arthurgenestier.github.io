5c69964f5f15026517879cadde4801c06d57dbe6
arthurgenestier <genestier.arthur@gmail.com>
2022-03-14 17:55:37 +0100

    ok

--- CAchute/assets/js/app.js
+++ CAchute/assets/js/app.js
@@ -1,28 +1,24 @@
 function addCheck() {
     var cardCheck = document.getElementById("firstCard");
     cardCheck.classList.add("check");
-    var cardCheckLine = document.getElementById("firstLine");
-    cardCheckLine.classList.add("container-line");
 } 
 function addCheck2() {
     var cardCheck = document.getElementById("secondCard");
     cardCheck.classList.add("check");
-    var cardCheckLine = document.getElementById("secondLine");
-    cardCheckLine.classList.add("container-line");
-    } 
+}
 function addCheck3() {
     var cardCheck = document.getElementById("thirdCard");
     cardCheck.classList.add("check");
-    } 
+} 
 function addCheck4() {
     var cardCheck = document.getElementById("fourthCard");
     cardCheck.classList.add("check");
-    } 
+} 
 function addCheck5() {
     var cardCheck = document.getElementById("fifthCard");
     cardCheck.classList.add("check");
-    } 
+} 
 function addCheck6() {
     var cardCheck = document.getElementById("sixthCard");
     cardCheck.classList.add("check");
-    } 
+} 
