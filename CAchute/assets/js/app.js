cb88289735cffc37828ec89e55203bda0aa11b3d
arthurgenestier <genestier.arthur@gmail.com>
2022-03-14 17:33:02 +0100

    plus que check line

--- CAchute/assets/js/app.js
+++ CAchute/assets/js/app.js
@@ -1,10 +1,14 @@
 function addCheck() {
     var cardCheck = document.getElementById("firstCard");
     cardCheck.classList.add("check");
-    } 
+    var cardCheckLine = document.getElementById("firstLine");
+    cardCheckLine.classList.add("container-line");
+} 
 function addCheck2() {
     var cardCheck = document.getElementById("secondCard");
     cardCheck.classList.add("check");
+    var cardCheckLine = document.getElementById("secondLine");
+    cardCheckLine.classList.add("container-line");
     } 
 function addCheck3() {
     var cardCheck = document.getElementById("thirdCard");
