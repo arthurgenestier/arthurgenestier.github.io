d22a0396acc2bd59caad36b0493d5ab9c9672ab9
arthurgenestier <genestier.arthur@gmail.com>
2022-03-14 16:13:22 +0100

    check card ok

---
+++ CAchute/assets/js/app.js
@@ -0,0 +1,24 @@
+function addCheck() {
+    var cardCheck = document.getElementById("firstCard");
+    cardCheck.classList.add("check");
+    } 
+function addCheck2() {
+    var cardCheck = document.getElementById("secondCard");
+    cardCheck.classList.add("check");
+    } 
+function addCheck3() {
+    var cardCheck = document.getElementById("thirdCard");
+    cardCheck.classList.add("check");
+    } 
+function addCheck4() {
+    var cardCheck = document.getElementById("fourthCard");
+    cardCheck.classList.add("check");
+    } 
+function addCheck5() {
+    var cardCheck = document.getElementById("fifthCard");
+    cardCheck.classList.add("check");
+    } 
+function addCheck6() {
+    var cardCheck = document.getElementById("sixthCard");
+    cardCheck.classList.add("check");
+    } 
