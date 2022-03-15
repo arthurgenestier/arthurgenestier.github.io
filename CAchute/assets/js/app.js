53b86d8093535550723fdc9998db9dcb0866db57
arthurgenestier <genestier.arthur@gmail.com>
2022-03-15 10:04:36 +0100

    steper ok

--- CAchute/assets/js/app.js
+++ CAchute/assets/js/app.js
@@ -1,24 +1,52 @@
 function addCheck() {
     var cardCheck = document.getElementById("firstCard");
     cardCheck.classList.add("check");
+    var line = document.getElementById("line");
+    line.classList.add("flex");
+    var lineCheck = document.getElementById("check-icon-first");
+    lineCheck.classList.add("block");
 } 
 function addCheck2() {
     var cardCheck = document.getElementById("secondCard");
     cardCheck.classList.add("check");
+    var line = document.getElementById("line");
+    line.classList.add("flex");
+    var lineCheck = document.getElementById("check-icon-second");
+    lineCheck.classList.add("block");
 }
 function addCheck3() {
     var cardCheck = document.getElementById("thirdCard");
     cardCheck.classList.add("check");
+    var line = document.getElementById("line");
+    line.classList.add("flex");
+    var lineCheck = document.getElementById("check-icon-third");
+    lineCheck.classList.add("block");
 } 
 function addCheck4() {
     var cardCheck = document.getElementById("fourthCard");
     cardCheck.classList.add("check");
+    var line = document.getElementById("line");
+    line.classList.add("flex");
+    var lineCheck = document.getElementById("check-icon-fourth");
+    lineCheck.classList.add("block");
 } 
 function addCheck5() {
     var cardCheck = document.getElementById("fifthCard");
     cardCheck.classList.add("check");
+    var line = document.getElementById("line");
+    line.classList.add("flex");
+    var lineCheck = document.getElementById("check-icon-fifth");
+    lineCheck.classList.add("block");
 } 
 function addCheck6() {
     var cardCheck = document.getElementById("sixthCard");
     cardCheck.classList.add("check");
-} 
+    var line = document.getElementById("line");
+    line.classList.add("flex");
+    var lineCheck = document.getElementById("check-icon-sixth");
+    lineCheck.classList.add("block");
+}
+function removeFlex() {
+    var cross = document.getElementById("line");
+    cross.classList.remove("flex");
+}
